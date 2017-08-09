3cac3fa77daf3830086cc819714eb6b0a66bc660
陈亚飞 <chenyafei@nongfenqi.com>
2017-08-09 16:48:30 +0800

    提交产品配件

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "babel-polyfill";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import { AppContainer } from "react-hot-loader";
 // AppContainer 是一个 HMR 必须的包裹(wrapper)组件
